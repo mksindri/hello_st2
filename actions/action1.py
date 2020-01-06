e5cba76430b99baf66028356d2c1f3d6d1e2a410
Mukesh Kumar_5375 <mukesh.kumar_5375@nihilent.com>
2020-01-06 22:33:35 +0530

    first commit

---
+++ actions/action1.py
@@ -0,0 +1,29 @@
+import eventlet
+
+from st2reactor.sensor.base import Sensor
+
+
+class Sensor1(Sensor):
+    def __init__(self, sensor_service, config):
+        super(Sensor1, self).__init__(sensor_service=sensor_service, config=config)
+        self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+        self._stop = False
+
+    def setup(self):
+        pass
+
+    def run(self):
+        pass
+
+    def cleanup(self):
+        self._stop = True
+
+    # Methods required for programmable sensors.
+    def add_trigger(self, trigger):
+        pass
+
+    def update_trigger(self, trigger):
+        pass
+
+    def remove_trigger(self, trigger):
+        pass
