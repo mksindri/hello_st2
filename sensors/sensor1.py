cb0216306b6c197cdd354433718c507a9c52bbb4
Mukesh Kumar_5375 <mukesh.kumar_5375@nihilent.com>
2020-01-07 10:51:43 +0530

    stop sensor

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -13,13 +13,14 @@ class HelloSensor(Sensor):
         pass
 
     def run(self):
-        while not self._stop:
-            self._logger.debug('HelloSensor dispatching trigger...')
-            count = self.sensor_service.get_value('hello_st2.count') or 0
-            payload = {'greeting': 'Yo, StackStorm!', 'count': int(count) + 1}
-            self.sensor_service.dispatch(trigger='hello_st2.event1', payload=payload)
-            self.sensor_service.set_value('hello_st2.count', payload['count'])
-            eventlet.sleep(60)
+        # while not self._stop:
+        #     self._logger.debug('HelloSensor dispatching trigger...')
+        #     count = self.sensor_service.get_value('hello_st2.count') or 0
+        #     payload = {'greeting': 'Yo, StackStorm!', 'count': int(count) + 1}
+        #     self.sensor_service.dispatch(trigger='hello_st2.event1', payload=payload)
+        #     self.sensor_service.set_value('hello_st2.count', payload['count'])
+        #     eventlet.sleep(60)
+        pass
 
     def cleanup(self):
         self._stop = True
